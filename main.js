e39a05e8a95757be971d466af946bce188d41ea7
Dario Jularic <dariojularic0@gmail.com>
2024-05-23 11:02:52 +0200

    Create form

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-import './style.css'
+// import './style.css'
+
+class Todo {
+  constructor(id, text) {
+      this.id = id;
+      this.text = text;
+  }
+
+  // get id() {
+  //   return this.id
+  // }
+}
+
+class TodoManager {
+  constructor(todoId) {
+    this.todoId = todoId;
+  }
+}
