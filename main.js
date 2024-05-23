5f905c339d4c955c02743cb6d7cf90f432337aa0
Dario Jularic <dariojularic0@gmail.com>
2024-05-23 20:17:45 +0200

    Refactor code

--- main.js
+++ main.js
@@ -3,47 +3,70 @@
 const form = document.querySelector("form")
 const inputText = document.querySelector("#todotext");
 const todoList = document.querySelector(".todo-list");
-const removeTask = document.querySelector(".remove-task");
+const deleteBtn = document.querySelector(".delete-btn");
+
 
 class Todo {
-  constructor(id, text) {
-      this.id = id;
+  constructor(text) {
+      this.id = self.crypto.randomUUID();
       this.text = text;
   }
 
- generateId() {
-  return self.crypto.randomUUID()
- }
+  // metoda changeColor random background color
 
 }
 
 class TodoManager {
-  constructor(todoList) {
-    this.todoList = todoList;
+  // dobra praksa 
+  todos;
+  constructor() {
+    this.todos = []
   }
   
-  filterTasks(taskId) {
-    	this.todoList
+  addTodo(todoTask) {
+    this.todos.push(todoTask)
   }
 
+  deleteTodo(todoList, todoId) {
+    this.todos = todoList.filter((todoTask) => {
+      todoTask.id !== todoId
+      console.log("id od todotask", todoTask.id)
+      console.log("id koji zelim izbrisat", todoId)
+      console.log(this.todos)
+    })
+  }
+
+  renderTodos() {
+    this.todos.forEach((todoTask) => {
+      const html = `<div class="todo-task">
+                    <p>${todoTask.text}</p>
+                    <button class="delete-btn" data-id="${todoTask.id}">Delete</button>
+                  </div>`;
+      todoList.insertAdjacentHTML("afterbegin", html)
+    })
+  }
 }
+
+const todoList1 = new TodoManager
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   // razlika izmedu == i ===??
   if (!inputText.value == "") {
-    const todo = new Todo(self.crypto.randomUUID(), inputText.value)
-    const html = `<div class="todo-task">
-                    <p>${todo.text}</p>
-                    <button class="remove-task">Delete</button>
-                  </div>`;
-    todoList.insertAdjacentHTML("afterbegin", html)
+    const todo = new Todo(inputText.value)
+    todoList1.addTodo(todo)
+    todoList.innerHTML = "";
+    todoList1.renderTodos()
     inputText.value = "";
-    console.log(Todo.generateId)
   }
 })
 
-// removeTask.addEventListener("click", () => {
-
-// })
+todoList.addEventListener("click", (event) => {
+  if (event.target.classList.contains("delete-btn")) {
+    console.log("todos array", todoList1.todos)
+    todoList1.deleteTodo(todoList1.todos, event.target.getAttribute("data-id"))
+    todoList1.renderTodos()
+    console.log("todos array", todoList1.todos)
+  }
+})
 
