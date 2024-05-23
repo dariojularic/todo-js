d7791ea9808f6d96a78bfe1b4459778fb9d8c79d
Dario Jularic <dariojularic0@gmail.com>
2024-05-23 16:59:48 +0200

    Add style

--- main.js
+++ main.js
@@ -1,18 +1,49 @@
 // import './style.css'
 
+const form = document.querySelector("form")
+const inputText = document.querySelector("#todotext");
+const todoList = document.querySelector(".todo-list");
+const removeTask = document.querySelector(".remove-task");
+
 class Todo {
   constructor(id, text) {
       this.id = id;
       this.text = text;
   }
 
-  // get id() {
-  //   return this.id
-  // }
+ generateId() {
+  return self.crypto.randomUUID()
+ }
+
 }
 
 class TodoManager {
-  constructor(todoId) {
-    this.todoId = todoId;
+  constructor(todoList) {
+    this.todoList = todoList;
   }
+  
+  filterTasks(taskId) {
+    	this.todoList
+  }
+
 }
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  // razlika izmedu == i ===??
+  if (!inputText.value == "") {
+    const todo = new Todo(self.crypto.randomUUID(), inputText.value)
+    const html = `<div class="todo-task">
+                    <p>${todo.text}</p>
+                    <button class="remove-task">Delete</button>
+                  </div>`;
+    todoList.insertAdjacentHTML("afterbegin", html)
+    inputText.value = "";
+    console.log(Todo.generateId)
+  }
+})
+
+// removeTask.addEventListener("click", () => {
+
+// })
+
